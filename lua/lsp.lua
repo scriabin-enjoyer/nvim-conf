abe0f606275eb8d20d157dd59ae4adfb7da6185d
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2025-04-25 13:43:31 -0400

    Change all lsp keymaps: prefix with 'l'

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -7,16 +7,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
             mode = mode or 'n'
             vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', 'Signature Help')
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('<leader>ld', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+        map('<leader>lD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('<leader>lt', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        map('<leader>lf', 'gf', '[G]oto File Under Cursor')
+        map('<leader>li', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map('<leader>lr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<cr>', 'Signature Help')
+        map('<leader>lh', vim.lsp.buf.hover, 'Hover Docs')
+        map('<leader>lds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        map('<leader>lws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('<leader>lR', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('<leader>lc', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
 
         -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
         ---@param client vim.lsp.Client
