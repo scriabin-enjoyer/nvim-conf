594365ecb821f3257a1ca7c0c8f6871d0ea07f05
Scriabin Enjoyer <197675111+scriabin-enjoyer@users.noreply.github.com>
2025-12-16 19:49:47 -0500

    Update: add logic for ruby-lsp startup with bundler

--- lua/custom-server-configs.lua
+++ lua/custom-server-configs.lua
@@ -19,7 +19,22 @@ local custom_servers = {
     },
     ruby_lsp = {
         -- autostart = false,
-        cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") },
+        root_dir = function() return vim.fn.getcwd() end,
+        -- cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") }, -- fallback
+        -- NOTE: on_new_config is an nvim-lspconfig specific option, it is not
+        -- a valid field in the vim.lsp.ClientConfig table as of time of writing
+        on_new_config = function(new_config, root_dir)
+            local gemfile_lock = root_dir .. "/Gemfile.lock"
+
+            if vim.fn.filereadable(gemfile_lock) == 1 then
+                print("Gemfile.lock found; Executing: bundle exec ruby-lsp")
+                new_config.cmd = { "bundle", "exec", "ruby-lsp" }
+            else
+                print("Executing: global ruby-lsp")
+                new_config.cmd = { vim.fn.expand("~/.rbenv/shims/ruby-lsp") }
+            end
+        end,
+        filetypes = { 'ruby' },
         single_file_support = true,
         init_options = {
             enabledFeatures = {
